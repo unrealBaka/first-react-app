7606a55fa81f0a0c7979d52ae2bf83a491b1c167
Vlad <49783048+unrealBaka@users.noreply.github.com>
2020-02-26 17:20:47 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import state from './redux/state';
 
 ReactDOM.render(<App  state = {state} />, document.getElementById('root'));
 
+//check for git-hub commit
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
