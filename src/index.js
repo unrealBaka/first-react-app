95472a60004c36e62c01e50487dbcf2071b585b7
Vlad <49783048+unrealBaka@users.noreply.github.com>
2020-03-03 16:10:23 +0200

    Add store object

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from './redux/state';
+import store from './redux/state';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost} from "./redux/state";
-import {updateNewPostText} from "./redux/state";
-import {updateNewMessageText} from "./redux/state";
-import {sendMessage} from "./redux/state";
+
 
 let reRenderEntireTree = (state) => {
 
-    ReactDOM.render(<App state={state} updateNewPostText={updateNewPostText} addPost={addPost}
-                         updateNewMessage={updateNewMessageText}
-                         sendMessage={sendMessage}/>, document.getElementById('root'));
+    ReactDOM.render(<App state={state} updateNewPostText={store.updateNewPostText.bind(store)} addPost={store.addPost.bind(store)}
+                         updateNewMessage={store.updateNewMessageText.bind(store)}
+                         sendMessage={store.sendMessage.bind(store)}/>, document.getElementById('root'));
 
 };
 
-reRenderEntireTree(state);
+reRenderEntireTree(store.getState());
 
-subscribe(reRenderEntireTree);
+store.subscribe(reRenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,83 +1,82 @@
-let reRenderEntireTree = () =>{
-    console.log("State was changed!");
-};
-
-let state = {
-    profilePage:{
-        posts:  [
-            {id: '0', message: 'My first post', likes: '150'},
-            {id: '1', message: 'I have a grate idea', likes: '10'},
-            {id: '2', message: 'Sun is shining', likes: '3'},
-            {id: '3', message: 'My heart is go ooooon', likes: '52'},
-            {id: '4', message: 'Turutut tu tut ut utu t', likes: '1500'},
-        ],
-        newPostText: "Vlads application"
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: '0', message: 'My first post', likes: '150'},
+                {id: '1', message: 'I have a grate idea', likes: '10'},
+                {id: '2', message: 'Sun is shining', likes: '3'},
+                {id: '3', message: 'My heart is go ooooon', likes: '52'},
+                {id: '4', message: 'Turutut tu tut ut utu t', likes: '1500'},
+            ],
+            newPostText: "Vlads application"
+        },
+        messagesPage: {
+            messages: [
+                {id: 1, message: 'Hi', type: '0'},
+                {
+                    id: 2,
+                    message: 'How are you?How are you?How are you?How are you?How are you?How are you?How are you?How are you?How are you?How are you?' +
+                        'How are you?How are you?How are you?How are you?',
+                    type: '1'
+                },
+                {id: 3, message: 'Are you okay?', type: '0'},
+                {id: 4, message: 'Lalalala', type: '1'},
+                {id: 5, message: 'LOL', type: '0'},
+                {id: 6, message: 'Privet pudr', type: '1'},
+                {id: 7, message: 'No no no!', type: '0'}
+            ],
+            dialogs: [
+                {id: 1, name: 'Rina'},
+                {id: 2, name: 'Vlad'},
+                {id: 3, name: 'Andrew'},
+                {id: 4, name: 'Claud'},
+                {id: 5, name: 'Alex'},
+                {id: 6, name: 'John'},
+                {id: 7, name: 'Egor'}
+            ],
+            newMessage: "New message"
+        }
     },
-    messagesPage: {
-        messages:  [
-            {id: 1, message: 'Hi', type:'0'},
-            {id: 2, message: 'How are you?How are you?How are you?How are you?How are you?How are you?How are you?How are you?How are you?How are you?' +
-                    'How are you?How are you?How are you?How are you?',type:'1'},
-            {id: 3, message: 'Are you okay?', type:'0'},
-            {id: 4, message: 'Lalalala', type:'1'},
-            {id: 5, message: 'LOL', type:'0'},
-            {id: 6, message: 'Privet pudr', type:'1'},
-            {id: 7, message: 'No no no!', type:'0'}
-        ],
-        dialogs:  [
-            {id: 1, name: 'Rina'},
-            {id: 2, name: 'Vlad'},
-            {id: 3, name: 'Andrew'},
-            {id: 4, name: 'Claud'},
-            {id: 5, name: 'Alex'},
-            {id: 6, name: 'John'},
-            {id: 7, name: 'Egor'}
-        ],
-        newMessage: "New message"
-    }
-}
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log("State was changed!");
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likes: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(PostText) {
+        this._state.profilePage.newPostText = PostText;
+        this._callSubscriber(this._state);
+    },
+    sendMessage() {
+        let newMessage = {
+            id: 8,
+            message: this._state.messagesPage.newMessage,
+            type: '1'
+        };
+        this._state.messagesPage.messages.push(newMessage);
+        this._state.messagesPage.newMessage = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newMessageText) {
+        this._state.messagesPage.newMessage = newMessageText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; //observer pattern
+    },
 
-window.state = state;
-
-//functions with myProfile and posts
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likes: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    reRenderEntireTree(state);
 };
 
-export const updateNewPostText = (PostText) => {
-    state.profilePage.newPostText = PostText;
-    reRenderEntireTree(state);
-};
 
-//functions with messages
-
-export const sendMessage = () => {
-    let newMessage = {
-        id: 8,
-        message: state.messagesPage.newMessage,
-        type:'1'
-    };
-    state.messagesPage.messages.push(newMessage);
-    state.messagesPage.newMessage = '';
-    reRenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newMessageText) => {
-    state.messagesPage.newMessage = newMessageText;
-    reRenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-    reRenderEntireTree = observer; //observer pattern
-};
-
-export default state;
-
-//store
+window.state = store;
+export default store;
